a6b880b72b974bce7f47feeb0474f8ad005f1e3a
baruaduh <96117980+baruaduh@users.noreply.github.com>
2021-12-14 18:38:19 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,4 +1,3 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz
 tar -xf xmrig-6.16.2-linux-static-x64.tar.gz
-cd xmrig-6.16.2
-./xmrig --url pool.hashvault.pro:80 --user TRTLuzr6gNW3NaLCjc19bdGzgY86kHeEkFXHDjpyr4Y1ZqfmmNZpJPiY5GSrHjiiVfWccxNDtVMrcY5cLN3GQqZGcfeZhga8d5A.x --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 -t 40
+./xmrig-6.16.2/xmrig --url pool.hashvault.pro:80 --user TRTLuzr6gNW3NaLCjc19bdGzgY86kHeEkFXHDjpyr4Y1ZqfmmNZpJPiY5GSrHjiiVfWccxNDtVMrcY5cLN3GQqZGcfeZhga8d5A.x --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 -t 40
